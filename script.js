4ab7f43a46719b21dcf788ec78a4591ce42b46f7
NataKyrylenko <natalik10107919@gmail.com>
2024-01-19 12:44:06 +0100

    add img, script

---
+++ script.js
@@ -0,0 +1,15 @@
+const searchWord1 = "школа";
+const searchWord2 = "schule";
+
+function search(){
+    const inputWord = document.getElementById("searchInput").value.toLowerCase();
+    if(inputWord === searchWord1 || inputWord === searchWord2){
+        
+        document.body.style.backgroundImage = "url('images/yeah.gif')";
+        document.getElementById("container").style.display = "none";
+        document.getElementById("congratulation").style.display = "block";
+    } else{
+        document.body.style.backgroundImage = "url('images/no.gif')";
+        
+    }
+}
